adb51fd3448beeac7f25a0cc496887fc77f02655
Muralitharan k <muralitharan805@gmail.com>
2020-11-13 18:39:39 +0530

    You can put any valid JavaScript expression inside the curly braces in JSX. For example, 2 + 2, user.firstName, or formatName(user) are all valid JavaScript expressions.
    In the example, embed the result of calling a JavaScript function, formatName(user), into an <h1> element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 
 
-const name = "react"
-const element = <h1>Hello, world!, this is {name}</h1>;
+
+function Name(user){
+  return user.firstname+' '+user.lastname
+}
+
+const user = {
+  firstname:"ramu",
+  lastname:'K'
+}
 function App() {
   return (
     <div className="App">
-      <h1>{element}</h1>
+      <h1>hello {Name(user)}</h1>
     </div>
   );
 }
