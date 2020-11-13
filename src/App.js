e2535288a8d73ca0e191b9fe1d886f0be3c9cc21
Muralitharan k <muralitharan805@gmail.com>
2020-11-13 18:32:17 +0530

    It is called JSX, and it is a syntax extension to JavaScript. Recommend using it with React to describe what the UI should look like,

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+
 function App() {
+const name = "react"
+const element = <h1>Hello, world!, this is {name}</h1>;
   return (
     <div className="App">
-      <h2>Helloff</h2>
+      <h1>{element}</h1>
     </div>
   );
 }
