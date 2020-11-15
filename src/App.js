8736076b57e5050dd7d0649e932734ca3058382f
Muralitharan k <muralitharan805@gmail.com>
2020-11-15 10:24:22 +0530

    Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React,{Component} from 'react'
 
-function App() {
+
+class Welcome_class extends React.Component{
+  render(){
+    return(
+      <h1>Hello {this.props.name} welcome this is class Component</h1>
+    )
+  }
+}
+
+function Welcome(props){
+  return(
+  <h1>Hello {props.name} welcome this is function component</h1>
+  )
+}
+
+
+function App(name) {
   return (
     <div className="App">
-      <h2>This is basic setup</h2>
+      <Welcome_class name="murali"></Welcome_class>
+      <Welcome name="murali"></Welcome>
     </div>
   );
 }
