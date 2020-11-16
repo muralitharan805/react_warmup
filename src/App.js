6bd61e7e76d4e3627c7f83390c0c397d31071da4
Muralitharan k <muralitharan805@gmail.com>
2020-11-16 14:53:51 +0530

    Conditional Rendraing simple example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props){
+  const isLoggedIn = props.isLoggedIn
+  if(isLoggedIn){
+    return <UserGreeting></UserGreeting>
+  }else{
+    return <GuestGreeting></GuestGreeting>
+
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>this is basic setup</h1>
+      <Greeting isLoggedIn={false}></Greeting>
     </div>
   );
 }
