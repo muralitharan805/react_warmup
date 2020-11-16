ac6d815a3ff27c54c3cb0776dd890c33a756772f
Muralitharan k <muralitharan805@gmail.com>
2020-11-16 11:28:47 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react'
 
+// class Time extends React.Component{
+//   render(){
+//   return(
+
+// <h2>It is {new Date().toLocaleTimeString()}</h2>
+//     )
+//   }
+// }
 function App() {
   return (
     <div className="App">
-      <h2>This is basic setup</h2>
+      <Time></Time>
     </div>
   );
 }
