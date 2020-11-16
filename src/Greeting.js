bf7894a0f1656400e53d79fe6293cf2fb5d4dbea
Muralitharan k <muralitharan805@gmail.com>
2020-11-17 00:04:47 +0530

    Seconde hook done

--- src/Greeting.js
+++ src/Greeting.js
@@ -4,14 +4,27 @@ import Row from './Row'
 
 export default function Greeting(props){
   const [name,setName] = useState("john")
+  const [surname,setsurName] = useState("sk")
   function handleOnchange(e){
     setName(e.target.value)
   }
+  function handleOnSurnamechange(e){
+    setsurName(e.target.value)
+  }
   return (
           <section>
+            <div>
+
             <Row label="Name">
             </Row>
             <input type='text' value={name} onChange={handleOnchange} ></input>
+            </div>
+            <div>
+
+            <Row label="Surname">
+            </Row>
+            <input type='text' value={surname} onChange={handleOnSurnamechange} ></input>
+            </div>
           </section>
         )
 }
