5377ee3a75ca873c388a0967155dad6c7263220e
Muralitharan k <muralitharan805@gmail.com>
2020-11-16 23:57:14 +0530

    First done

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,33 +1,6 @@
 import React, { useState } from 'react'
 import Row from './Row'
 
-// export default class Greeting extends React.Component{
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       name:"johm"
-//     }
-
-//     this.onHandleChange=this.onHandleChange.bind(this)
-//   }
-
-//   onHandleChange(e){
-//     this.setState({
-//       name:e.target.value
-//     })
-//   }
-//   render(){
-//     return (
-      
-//       <section>
-       
-//         <Row label="Name">
-//         </Row>
-//         <input type='text' value={this.state.name} onChange={this.onHandleChange}></input>
-//       </section>
-//     )
-//   }
-// }
 
 export default function Greeting(props){
   const [name,setName] = useState("john")
@@ -35,9 +8,7 @@ export default function Greeting(props){
     setName(e.target.value)
   }
   return (
-      
           <section>
-           
             <Row label="Name">
             </Row>
             <input type='text' value={name} onChange={handleOnchange} ></input>
