8b85326aa36de04fab2dc9d2105ed7c5aaabad41
Muralitharan k <muralitharan805@gmail.com>
2020-11-16 20:34:21 +0530

    Control Component Text Box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import React from 'react'
 
+
+
+class NameFrom extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      value:''
+    }
+    this.handleOnSubmit=this.handleOnSubmit.bind(this)
+    this.handleOnchange=this.handleOnchange.bind(this)
+  }
+
+  handleOnchange(event){
+    this.setState({
+      value:event.target.value
+    })
+
+    console.log(this.state.value);
+  }
+  handleOnSubmit(event){
+    
+    event.preventDefault()
+  }
+  render(){
+    return(
+      <div>
+        <br/>
+        <form onSubmit={this.handleOnSubmit} >
+        <label>
+          Name: 
+          <input type='text' value={this.state.value} onChange={this.handleOnchange} ></input>
+          <input type='submit' value='submit' ></input>
+        </label>
+        </form>
+      </div>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
-      <h1>this is basic setup</h1>
+      <NameFrom></NameFrom>
     </div>
   );
 }
