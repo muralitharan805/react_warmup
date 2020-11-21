3c7e277ea7f7bac0e86f360a459098160172d7a2
Muralitharan k <muralitharan805@gmail.com>
2020-11-21 23:59:52 +0530

    Copostion done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+
+// Some components don’t know their children ahead of time. This is especially common for components like Sidebar or Dialog that represent generic “boxes”.
+
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>this is basic setup</h1>
+      <FancyBorder color="green">
+
+      <h1>this is React Compostion</h1>
+      </FancyBorder>
     </div>
   );
 }
