5cde0014dc630915934e02ea73a190ca424d6ee3
Muralitharan k <muralitharan805@gmail.com>
2020-11-21 19:05:19 +0530

    .

--- src/Greeting.js
+++ src/Greeting.js
@@ -5,6 +5,20 @@ import Row from './Row'
 export default function Greeting(props){
   const [name,setName] = useState("john")
   const [surname,setsurName] = useState("sk")
+
+  const [width,setWidth] = useState(window.innerWidth)
+
+  function handleResize(){
+    setWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize',handleResize)
+    return () => {
+      window.removeEventListener('resize',handleResize)
+
+    }
+  })
+
   function handleOnchange(e){
     setName(e.target.value)
   }
@@ -33,6 +47,14 @@ export default function Greeting(props){
             </Row>
             <input type='text' value={surname} onChange={handleOnSurnamechange} ></input>
             </div>
+
+            <div>
+
+            <Row label="Windos With checking">
+            </Row>
+            {width}
+            </div>
+
           </section>
         )
 }
