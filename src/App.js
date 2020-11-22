8f52ff4aa96f5caf966be6985cc1e7bde1eec797
Muralitharan k <muralitharan805@gmail.com>
2020-11-22 22:47:08 +0530

    Fragment one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import FragmentDemo from './FragmentDemo'
 function App() {
   return (
     <div className="App">
       <h1>this is basic setup</h1>
+      <FragmentDemo></FragmentDemo>
     </div>
   );
 }
---
+++ src/FragmentDemo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FragmentDemo() {
+  return (
+    <React.Fragment>
+      <h1>Fragment Deo</h1>
+
+    <p>ss</p>
+    </React.Fragment>
+  )
+}
+
+export default FragmentDemo
