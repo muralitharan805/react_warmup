f08b27e712e70c745a09faff0f71a63540a79f9f
Muralitharan k <muralitharan805@gmail.com>
2020-11-23 23:19:08 +0530

    ,

---
+++ src/DataFetch.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+function DataFetch() {
+  const [posts,setPosts]= useState([])
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(
+      res=>{
+        console.log(res);
+        setPosts(res.data)
+      }
+    ).catch(error=>{
+      console.log(error);
+    })
+  },[])
+  return (
+    <div>
+      <ul>
+        {
+          posts.map(post=>(
+            <li key={post.id}>
+              {
+                post.title
+              }
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default DataFetch
