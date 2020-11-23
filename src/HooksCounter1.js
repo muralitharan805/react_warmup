a553b9d8219c47fb753ca41b131f1e67b5a7fb34
Muralitharan k <muralitharan805@gmail.com>
2020-11-23 18:49:55 +0530

    compare values inlifecyle methods inside hooks

--- src/HooksCounter1.js
+++ src/HooksCounter1.js
@@ -4,15 +4,19 @@ function HooksCounter1() {
 
   const [count,setCount]=useState(0)
 
+  const [name,setName]=useState('')
+
   useEffect(() => {
-    
+    console.log("Useeffect update");
     document.title=`Click 
     ${count} time Hooks`
 
      
-  })
+  },[count])
   return (
     <div>
+      <h1>Hooks useEffect</h1>
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)}></input>
       <button onClick={()=>setCount(count+1)}>Click {count}</button>
 
     </div>
